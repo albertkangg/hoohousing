5536a2debef443eff066bf9c4d6ebefc430b9d3d
Albert <albertkaang@gmail.com>
2024-06-04 13:22:56 -0400

    fix double navbar

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
