71db2cbc153cfb90f822a6ee9feceec6ac0568d1
Albert <albertkaang@gmail.com>
2024-06-04 16:10:07 -0400

    updated w cards

--- test-app/src/components/pages/Home.js
+++ test-app/src/components/pages/Home.js
@@ -2,11 +2,13 @@ import "../../App.css";
 import Navbar from "../Navbar";
 import Hero from "../Hero";
 import React from "react";
+import Cards from "../Cards";
 
 function Home() {
   return (
     <>
       <Hero />
+      <Cards />
     </>
   );
 }
