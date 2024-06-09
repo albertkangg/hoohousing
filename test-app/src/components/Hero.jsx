1d860eb4f1f1337da938e1ce96a72b7c13d5e7b4
Evan Zhang <2023ezhang3@tjhsst.edu>
2024-06-09 14:05:55 -0400

    added location tracking
    Console prints out the latittue and longtitude of device's IP address.

--- test-app/src/components/Hero.jsx
+++ test-app/src/components/Hero.jsx
@@ -1,7 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { hero } from '../assets';
 
 const Hero = () => {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    };
+    function success(pos) {
+        var crd = pos.coords;
+        console.log("Your current position is:");
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+    }
+    
+    function errors(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    useEffect(() => {
+        if (navigator.geolocation) {
+        navigator.permissions
+            .query({ name: "geolocation" })
+            .then(function (result) {
+            console.log(result);
+            if (result.state === "granted") {
+                //If granted then you can directly call your function here
+                navigator.geolocation.getCurrentPosition(success, errors, options);
+            } else if (result.state === "prompt") {
+                //If prompt then the user will be asked to give permission
+                navigator.geolocation.getCurrentPosition(success, errors, options);
+            } else if (result.state === "denied") {
+                //If denied then you have to show instructions to enable location
+            }
+            });
+        } else {
+        console.log("Geolocation is not supported by this browser.");
+        }
+    }, []);
+
     const [searchTerm, setSearchTerm] = useState('');
 
     const handleSearch = (event) => {
